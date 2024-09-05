8b3ca614c7285a28f51aead959c7eab0519b65d5
Peter Lakatos <peter.lakatos@outlook.de>
2024-09-05 23:30:50 +0200

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'start-overview', loadChildren: () => import('./pages/start-overview/start-overview.module').then(m => m.StartOverviewModule) },
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: '' ,redirectTo: 'start-overview', pathMatch: 'full' },
+  { path: '**', redirectTo: 'start-overview' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
