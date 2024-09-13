adecec8f0c6745092effcbab87ff73139b7c00e4
Peter Lakatos <peter.lakatos@tecvia.com>
2024-09-13 16:05:55 +0200

    update angular material / validator login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SpinnerService } from 'src/app/services/spinner/spinner.service';
 
 @Component({
@@ -6,13 +7,35 @@ import { SpinnerService } from 'src/app/services/spinner/spinner.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor(public spinnerService: SpinnerService) { }
+  constructor(public spinnerService: SpinnerService, public fb:FormBuilder) { }
+
+  public loginForm!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.initializeLoginForm();
+  }
 
 
   public navigateGuest(): void {
     this.spinnerService.loadSpinner('/register');
+  }
+
+
+  private initializeLoginForm(): void {
+      this.loginForm = this.fb.group({
+        email: ['',Validators.compose([Validators.required, Validators.email,Validators.minLength(5),Validators.maxLength(50)])],
+        password: ['',Validators.compose([Validators.required,Validators.minLength(3),Validators.maxLength(20)])],
+      });
+  }
+
+
+  public signIn(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
   }
 
 }
