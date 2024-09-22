2679a56aefe036ddcce583bf32c9f36aadb3f639
Peter Lakatos <peter.lakatos@tecvia.com>
2024-09-22 11:04:16 +0200

    add income functionalitys

--- src/app/pages/output-overview/components/output/output.component.ts
+++ src/app/pages/output-overview/components/output/output.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { Title } from '@angular/platform-browser';
 import { WhitchEdition } from 'src/app/models/whitch-edition.model';
 
@@ -10,6 +11,7 @@ import { WhitchEdition } from 'src/app/models/whitch-edition.model';
 export class OutputComponent implements OnInit {
   currentTitle: string = "";
   public inputOrOutput: string[] = ['Output','Input'];
+  public isMoneyActive = false;
 
   public whichEdition:WhitchEdition[] = [
     {id:'1',title:'Hörnlein',value:20,thePurposeOf:'The purpose of Edition 1',ibahn:'IBAN 1',since:new Date(),recipient:'Recipient 1',kind:'Debts'},
@@ -45,6 +47,31 @@ export class OutputComponent implements OnInit {
     console.log('click from output component');
   }
 
+  public changeInputsOutput(value: Event): void {
+    const selectElement = value.target as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+    console.log('Selected value:', selectedValue);
+  }
+
+  public clearInput(): void {
+    this.value = '';
+  }
+
+
+  public readMoney(money: number): void {
+    if(this.value === ''){
+      money = 0;
+    }
+    console.log('Read money', money);
+  }
+
+  public changeKindSelection(selectedId: string): void {
+    const selectedItem = this.whichEdition.find(item => item.id === selectedId);
+    if (selectedItem) {
+        selectedItem.value === 0 ? this.isMoneyActive = true : this.isMoneyActive = false;
+        console.log('Selected item:', selectedItem);
+    }
+  }
 
   /*
     It should be checked whether value is 0, if so then show how much input, otherwise hide it
